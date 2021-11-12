66d9c035e21a4739978194876c014febe73f3047
Ekrem Ekici <65715006+ekremekc@users.noreply.github.com>
2021-11-12 17:49:53 +0000

    Degeneracy introduced

--- helmholtz_x/geometry_pkgx/shape_derivatives_x.py
+++ helmholtz_x/geometry_pkgx/shape_derivatives_x.py
@@ -5,179 +5,106 @@ import scipy.linalg
 import ufl
 from helmholtz_x.helmholtz_pkgx.petsc4py_utils import conjugate_function
 # Take the directional derivative of f in the facet normal direction, Dn(f) := dot(grad(f), n).
-from ufl import  FacetNormal
+from ufl import  FacetNormal, grad, dot, inner
 from ufl.operators import Dn 
-grad = ufl.grad
-dot = ufl.dot
-div = ufl.div
-inner = ufl.inner
-# ________________________________________________________________________________
 
 
 
 def _shape_gradient_Dirichlet(c, p_dir, p_adj):
-    
-    return - c**2 * inner(grad(conjugate_function(p_adj)), grad(p_dir))
+    # Equation 4.34 in thesis
+    return - c**2 * Dn(conjugate_function(p_adj)) * Dn (p_dir)
 
 
 def _shape_gradient_Neumann(c, omega, p_dir, p_adj):
-    
-    return c**2 * inner(grad(conjugate_function(p_adj)), grad(p_dir)) - omega**2 * conjugate_function(p_adj) * p_dir
+    # Equation 4.35 in thesis
+    p_adj_conj = conjugate_function(p_adj)
+    return c**2 * dot(grad(p_adj_conj), grad(p_dir)) - omega**2 * p_dir * p_adj_conj
 
 
 
 def _shape_gradient_Robin(geometry, c, omega, p_dir, p_adj, index):
- 
-    # V = dolfinx.FunctionSpace(mesh, 'CG', 1)
-    # c = dolfinx.interpolate(c, V)
 
     # Equation 4.33 in thesis
     curvature = geometry.get_curvature_field(index)
     G = -conjugate_function(p_adj) * (curvature * c ** 2 + c * Dn(c)*Dn(p_dir)) + \
         _shape_gradient_Neumann(c, omega, p_dir, p_adj) + \
-            2 * _shape_gradient_Dirichlet(c, p_dir, p_adj)
+         2 * _shape_gradient_Dirichlet(c, p_dir, p_adj)
 
     return G
 
 # ________________________________________________________________________________
 
-def shape_derivatives(geometry, boundary_conditions, omega, p_dir, p_adj, c, local=False):
+def ShapeDerivativesParametric(geometry, boundary_conditions, omega, p_dir, p_adj, c, local=False):
 
     mesh = geometry.mesh
     facet_tags = geometry.facet_tags
 
     n = FacetNormal(mesh)
     
-    ds = ufl.Measure('ds', subdomain_data=facet_tags)
-
-    G_Dir = []
-    G_Neu = []
-    G_Rob = []
+    ds = ufl.Measure('ds', domain = mesh, subdomain_data = facet_tags)
 
     results = {}
-
-    # shape gradient, ufl forms for Dirichlet and Neumann boundary conditions
-
-    if 'Dirichlet' in boundary_conditions.values():
-        
-        G_Dir.append(_shape_gradient_Dirichlet(c, p_dir, p_adj))
-
-    if 'Neumann' in boundary_conditions.values():
-        G_Neu.append(_shape_gradient_Neumann(c, omega, p_dir, p_adj))
-
-    
 
     for i, value in boundary_conditions.items():
         
         if i in geometry.ctrl_pts:
-            if value == 'Dirichlet':
-                G = G_Dir
-            elif value == 'Neumann':
-                G = G_Neu
-            # elif list(value.keys())[0] == 'Robin':
+            if value == {'Dirichlet'}:
+                G = _shape_gradient_Dirichlet(c, p_dir, p_adj)
+            elif value == {'Neumann'}:
+                G = _shape_gradient_Neumann(c, omega, p_dir, p_adj)
             else :
-                G_Rob.append(_shape_gradient_Robin(geometry, c, omega, p_dir, p_adj, i))
-                G = G_Rob
+                G = _shape_gradient_Robin(geometry, c, omega, p_dir, p_adj, i)
+                
             
-            
-            
-            derivatives = np.zeros((len(geometry.ctrl_pts[i])), dtype=complex)
-            # print("LENGTH OF CONTROL POINTS: ", len(geometry.ctrl_pts[i]))
+            derivatives = np.zeros((len(geometry.ctrl_pts[i]),2), dtype=complex)
+
             for j in range(len(geometry.ctrl_pts[i])):
 
-                V = geometry.get_displacement_field(i,j)
+                V_x, V_y = geometry.get_displacement_field(i,j)
 
-                C = inner(V, n) # Displacements only y direction
+                derivatives[j][0] = assemble_scalar( inner(V_x, n) * G * ds(i) )
+                derivatives[j][1] = assemble_scalar( inner(V_y, n) * G * ds(i) )
                 
-                g = assemble_scalar(inner(C, G[0]) * ds(i)) # assemble_scalar(C * G[0] * ds(i)) gives different sign for imag part
-                
-                    # print("g is: ",g[0], )
-                if type(g) == complex:
-                    # the eigenvalue is simple
-                    derivatives[j] = g
-                elif len(g) == 4:
+ 
+            results[i] = derivatives
+            
+    return results
+
+
+def ShapeDerivativesDegenerate(geometry, boundary_conditions, omega, 
+                               p_dir1, p_dir2, p_adj1, p_adj2, c):
+
+    mesh = geometry.mesh
+    facet_tags = geometry.facet_tags
+    
+    n = FacetNormal(mesh)
+    ds = ufl.Measure('ds', domain = mesh, subdomain_data = facet_tags)
+
+    Q = dolfinx.FunctionSpace(geometry.mesh, ("CG", 1))
+    C = dolfinx.Constant(geometry.mesh,1)
+    C = dolfinx.interpolate(C, Q)
+
+    for i, value in boundary_conditions.items():
+        A = assemble_scalar(C * ds(i))
+        C = 1 / A
+        ## BELOW THIS LINE IS NOT IMPLEMENTED YET!
+        if value == {'Dirichlet'}:
+            #DO DEGENERACY
+            G = _shape_gradient_Dirichlet(c, p_dir1, p_adj1)
+        elif value == {'Neumann'}:
+            #DO DEGENERACY
+            G = _shape_gradient_Neumann(c, omega, p_dir1, p_adj1)
+        else :
+            #DO DEGENERACY
+            G = _shape_gradient_Robin(geometry, c, omega, p_dir1, p_adj1, i)
+
+        if len(G) == 4:
                     # the eigenvalues are 2-fold degenerate
-                    A = np.array(([g[0], g[1]],
-                                [g[2], g[3]]))
+                    A = np.array(([G[0], G[1]],
+                                [G[2], G[3]]))
                     eig = scipy.linalg.eigvals(A)
                     derivatives[j] = eig.tolist()
-                results[i] = derivatives
-            
-    return results
-
-def shape_dirichlet(geometry, boundary_conditions, omega, p_dir, p_adj, c, index):
     
-    mesh = geometry.mesh
-    facet_tags = geometry.facet_tags
-
-    n = FacetNormal(mesh)
-    
-    ds = ufl.Measure('ds', subdomain_data=facet_tags)
-    
-    G = _shape_gradient_Dirichlet(c, p_dir, p_adj)
-
-    results = {}
-
-    derivatives = np.zeros((len(geometry.ctrl_pts[index])), dtype=complex)
-    
-    for j in range(len(geometry.ctrl_pts[index])):
-
-            V = geometry.get_displacement_field(index,j)
-
-            C = inner(V, n) # Displacements only y direction
-            
-            g = assemble_scalar(inner(C, G) * ds(index))
-            
-                # print("g is: ",g[0], )
-            if type(g) == complex:
-                # the eigenvalue is simple
-                derivatives[j] = g
-            elif len(g) == 4:
-                # the eigenvalues are 2-fold degenerate
-                A = np.array(([g[0], g[1]],
-                            [g[2], g[3]]))
-                eig = scipy.linalg.eigvals(A)
-                derivatives[j] = eig.tolist()
-            results[index] = derivatives
-    return results
-
-def shape_neumann(geometry, boundary_conditions, omega, p_dir, p_adj, c, index):
-    
-    mesh = geometry.mesh
-    facet_tags = geometry.facet_tags
-
-    n = FacetNormal(mesh)
-    
-    ds = ufl.Measure('ds', subdomain_data=facet_tags)
-    
-    G = _shape_gradient_Neumann(c, omega, p_dir, p_adj)
-
-    results = {}
-
-    derivatives = np.zeros((len(geometry.ctrl_pts[index])), dtype=complex)
-    
-    for j in range(len(geometry.ctrl_pts[index])):
-
-            V = geometry.get_displacement_field(index,j)
-
-            C = inner(V, n) # Displacements only y direction
-            
-            g = assemble_scalar(inner(C, G) * ds(index))
-            
-                # print("g is: ",g[0], )
-            if type(g) == complex:
-                # the eigenvalue is simple
-                derivatives[j] = g
-            elif len(g) == 4:
-                # the eigenvalues are 2-fold degenerate
-                A = np.array(([g[0], g[1]],
-                            [g[2], g[3]]))
-                eig = scipy.linalg.eigvals(A)
-                derivatives[j] = eig.tolist()
-            results[index] = derivatives
-    return results
-
-
 
 
 
